7b7383964136343a6c7c84778bc7224ecfb73e29
Jun <zhoujun97099@gmail.com>
2023-03-01 19:32:25 +0100

    imported retrofit libraries into build.gradle and created APIRestService interface(not finished)

---
+++ app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/retrofitutils/APIRestService.java
@@ -0,0 +1,10 @@
+package com.example.proyectoa_pmdm_t2_junzhou.retrofitutils;
+
+import retrofit2.http.GET;
+
+public interface APIRestService {
+    public static final String BASE_URL = "https://datos.madrid.es/";
+
+
+
+}
