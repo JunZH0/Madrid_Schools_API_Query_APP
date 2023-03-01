dcc150fe0b115a822e57f03c5bc4b22c201b1524
Jun <zhoujun97099@gmail.com>
2023-03-01 23:20:41 +0100

    implemented RetrofitClient class. Added UI items to MainActivity and created initRetrofit method

--- app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/MainActivity.java
+++ app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/MainActivity.java
@@ -4,13 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.proyectoa_pmdm_t2_junzhou.retrofitutils.APIRestService;
+import com.example.proyectoa_pmdm_t2_junzhou.retrofitutils.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTitulo;
+    TextView tvFiltro;
+    TextView tvFiltro2;
+    TextView tvFiltro3;
+    Button btnConsultar;
+    Button btnFiltro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTitulo = findViewById(R.id.tvTitulo);
+        tvFiltro = findViewById(R.id.tvFiltro);
+        tvFiltro2 = findViewById(R.id.tvFiltro2);
+        tvFiltro3 = findViewById(R.id.tvFiltro3);
+        btnConsultar = findViewById(R.id.btn_consulta);
+        btnFiltro = findViewById(R.id.btn_filtro);
+
+        initRetrofit();
+
+
+    }
+
+    private void initRetrofit() {
+        Retrofit retrofit = RetrofitClient.getClient(APIRestService.BASE_URL);
+        APIRestService ars = retrofit.create(APIRestService.class);
 
     }
 
--- app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/retrofitutils/APIRestService.java
+++ app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/retrofitutils/APIRestService.java
@@ -5,5 +5,6 @@ import retrofit2.http.GET;
 public interface APIRestService {
     public static final String BASE_URL = "https://datos.madrid.es/";
 
+    // TODO @GET
 
 }
