ccbfcd74b89b490af0f8d50d45f9ea372d49b637
Jun <zhoujun97099@gmail.com>
2023-03-03 22:29:34 +0100

    changed FilterDialog buttons to a onShotListener

--- app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/FilterDialog.java
+++ app/src/main/java/com/example/proyectoa_pmdm_t2_junzhou/FilterDialog.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -23,7 +24,7 @@ public class FilterDialog extends DialogFragment {
 
     @NonNull
     @Override
-    public Dialog onCreateDialog( Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View v = getActivity().getLayoutInflater().inflate(R.layout.activity_filter_dialog, null);
         builder.setView(v);
@@ -33,49 +34,55 @@ public class FilterDialog extends DialogFragment {
         etDist = v.findViewById(R.id.etDist);
 
 
+      // onShowListener para que no se pueda cerrar el dialogo hasta que se rellenen todos los campos
+        builder.setTitle("Seleccionar filtro").setPositiveButton("Aceptar", null).setNegativeButton("Cancelar", null);
 
-        // TODO onShowListener para que no se pueda cerrar el dialogo hasta que se rellenen todos los campos
-        builder.setTitle("Seleccionar filtro").setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String lat = etLat.getText().toString();
-                String lon = etLon.getText().toString();
-                String dist = etDist.getText().toString();
+            public void onShow(DialogInterface dialogInterface) {
+                Button button = ((AlertDialog) dialogInterface).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String lat = etLat.getText().toString();
+                        String lon = etLon.getText().toString();
+                        String dist = etDist.getText().toString();
 
-                if (lat.isEmpty() || lon.isEmpty() || dist.isEmpty()) {
-                    dialog.cancel();
-                    Toast.makeText(getActivity(), "No puede haber campos vacíos", Toast.LENGTH_SHORT).show();
-                } else {
-                    if(listener != null) {
-                        listener.onDatosListener(Double.parseDouble(lat), Double.parseDouble(lon), Integer.parseInt(dist));
+                        if (lat.isEmpty() || lon.isEmpty() || dist.isEmpty()) {
+                            // Show a message to the user that there are empty fields
+                            Toast.makeText(getActivity(), "No puede haber campos vacíos", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Call the listener only if there is data
+                            if (listener != null) {
+                                listener.onDatosListener(Double.parseDouble(lat), Double.parseDouble(lon), Integer.parseInt(dist));
+                            }
+                            dialog.dismiss();
+                        }
                     }
-                }
-
-            }
-        } ).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
+                });
             }
         });
-        return builder.create();
+        return dialog;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-            listener = (OnDatosListener) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString() + " must implement OnDatosListener");
-        }
-    }
 
-    @Override
-    public void onDetach() {
-        if (listener != null) {
-            listener = null;
+        @Override
+        public void onAttach (Context context){
+            super.onAttach(context);
+            try {
+                listener = (OnDatosListener) context;
+            } catch (ClassCastException e) {
+                throw new ClassCastException(context.toString() + " must implement OnDatosListener");
+            }
         }
-        super.onDetach();
+
+
+        @Override
+        public void onDetach () {
+            if (listener != null) {
+                listener = null;
+            }
+            super.onDetach();
+        }
     }
-}
